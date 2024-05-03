7e0b2a203c46de33c708343146690fb0f7b93364
chidoriii7 <muratore.danilo@yahoo.it>
2024-05-03 13:29:33 +0200

    jsnack 3 - corretto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-let risultato = 0
+let risultato = 0;
 
 // creare un ciclo che gira 10 volte
 for(let i = 0 ; i < 10; i++) {
@@ -9,8 +9,10 @@ for(let i = 0 ; i < 10; i++) {
     // chiedere all'utente di inserire un numero ogni volta che il ciclo si ripete (10 volte)
     // fare la somma di tutti i numeri inseriti
 
-    risultato = risultato + Number(prompt('Inserisci un numero'));
+    let numeroInserito = Number(prompt('Inserisci un numero'));
+    console.log(numeroInserito);
+    risultato = risultato + numeroInserito ;
     
 }
 
-console.log(risultato)
+console.log(risultato);
