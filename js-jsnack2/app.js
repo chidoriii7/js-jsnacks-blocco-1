5c080ada1f78d1387e48d87b3c88e0b47d147510
chidoriii7 <muratore.danilo@yahoo.it>
2024-05-03 12:28:52 +0200

    jsnack2 - corretto

--- js-jsnack2/app.js
+++ js-jsnack2/app.js
@@ -2,9 +2,9 @@
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
 // - fare inserire all'utente una prima parola
-let parola1 = prompt('inserisci la prima parola')
+const parola1 = prompt('inserisci la prima parola');
 // - fare inserire all'utente una seconda parola
-let parola2 = prompt('inserisci la seconda parola')
+const parola2 = prompt('inserisci la seconda parola');
 
 // - effettuare un controllo sulla lunghezza di entrambe la parole
 
@@ -12,13 +12,17 @@ let parola2 = prompt('inserisci la seconda parola')
 //     -stampare come seconda la parola più lunga
 if (parola1.length < parola2.length) {
 
-    console.log('la parola più corta è:', parola1, (parola1.length))
-    console.log('la parola più lunga è:', parola2, (parola2.length))
+    console.log('la parola più corta è:', parola1, (parola1.length));
+    console.log('la parola più lunga è:', parola2, (parola2.length));
 
-} else {
+} else if (parola2.length < parola1.length) {
 
-    console.log('la parola più corta è:', parola2, (parola2.length))
-    console.log('la parola più lunga è:', parola1, (parola1.length))
+    console.log('la parola più corta è:', parola2, (parola2.length));
+    console.log('la parola più lunga è:', parola1, (parola1.length));
     
+} else {
+    
+    console.log('le parole hanno la stessa lunghezza');
+
 }
 
